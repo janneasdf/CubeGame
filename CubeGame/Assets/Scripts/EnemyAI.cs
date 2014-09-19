02aecfe00b594546fa0f847d7f8902cea91330ae
Janne Gröndahl <janne.grondahl@aalto.fi>
2014-09-19 23:04:56 +0300

    Create enemy material change on disable powerup

---
+++ CubeGame/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public GameObject player;
+	public float forceFactor;
+	public GameController gameController;
+    public Material disabledMaterial;
+
+    float disabledTime = 0.0f;
+    Material originalMaterial;
+
+	// Use this for initialization
+	void Start () {
+        originalMaterial = renderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -2.0f) {
+			gameController.RegisterEnemyDeath ();
+			Destroy (gameObject);
+			return;
+		}
+
+		disabledTime -= Time.deltaTime;
+		if (disabledTime <= 0.0f) {
+            if (disabledTime + Time.deltaTime >= 0.0f)
+            {
+                renderer.material = originalMaterial;
+            }
+			Vector3 playerPos = player.transform.position;
+			Vector3 force = (playerPos - transform.position).normalized * forceFactor;
+			force.y = 0.0f;
+			rigidbody.AddForce (force);
+		}
+	}
+
+	public void DisableMovement(float time) {
+		disabledTime = time;
+        renderer.material = disabledMaterial;
+	}
+}
---
+++ CubeGame/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public float forceFactor;
+	public GameController gameController;
+	public int powerLeft = 1;		// ability
+	public float power;
+	public float powerRadius;		// not used atm
+	public float enemyDisableTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -6.0f) {
+			gameController.RegisterPlayerDeath ();
+			return;
+		}
+
+		Vector3 input = new Vector3 ();
+		if (Input.GetKey (KeyCode.A))
+			input.x -= 1;
+		if (Input.GetKey (KeyCode.D))
+			input.x += 1;
+		if (Input.GetKey (KeyCode.W))
+			input.z += 1;
+		if (Input.GetKey (KeyCode.S))
+			input.z -= 1;
+		input.Normalize();
+
+		if (Input.GetKeyDown (KeyCode.Space) && powerLeft > 0) {
+			foreach (GameObject enemy in GameObject.FindGameObjectsWithTag(GameController.enemyTag)) {
+				Vector3 force = (enemy.transform.position - transform.position).normalized * power;
+				enemy.rigidbody.AddForce(force);
+				enemy.GetComponent<EnemyAI>().DisableMovement(enemyDisableTime);
+			}
+			powerLeft--;
+		}
+
+		rigidbody.AddForce (input * forceFactor);
+	}
+}
